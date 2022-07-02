ed5b07d490fa015697e2d32798cfe47569d23c34
olof-sky <oloof92@gmail.com>
2022-07-02 21:13:06 +0200

    Change route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,34 @@
 import { createWebHistory, createRouter } from "vue-router";
 import WelcomePage from "@/views/WelcomePage.vue";
 
+import ContactPage from "@/views/WelcomePageViews/ContactPage.vue";
+
+import CodefolioPage from "@/views/PortfolioViews/CodefolioPage.vue";
+import ArtfolioPage from "@/views/PortfolioViews/ArtfolioPage.vue";
+
 const routes = [
   {
-    path: "/about",
+    path: "/",
     name: "WelcomePage",
     component: WelcomePage,
+  },
+
+  {
+    path: "/artfolio",
+    name: "ArtfolioPage",
+    component: ArtfolioPage,
+  },
+
+  {
+    path: "/codefolio",
+    name: "CodelioPage",
+    component: CodefolioPage,
+  },
+
+  {
+    path: "/contact",
+    name: "ContactPage",
+    component: ContactPage,
   },
 ];
 
