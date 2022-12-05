a4bfb26cbb7897b65140e79b163bc58995817c56
Sky <oloof92@gmail.com>
2022-12-05 14:31:50 +0100

    Update dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <template>
-<head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Almarai&family=Gothic+A1&family=Questrial&display=swap" rel="stylesheet">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Almarai&family=Gothic+A1&family=Montserrat:wght@400&family=Questrial&display=swap" rel="stylesheet">
-</head>
-  <NavBar/>
-  <router-view/>
+  <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Almarai&family=Gothic+A1&family=Questrial&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Almarai&family=Gothic+A1&family=Montserrat:wght@400&family=Questrial&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <NavBar />
+  <router-view />
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue';
+  import NavBar from "./components/NavBar.vue";
 
-export default {
+  export default {
+    name: "App",
+    data() {
+      return {};
+    },
 
-  name: 'App',
-  data () {
-    return {
-    }
-  },
-
-  components: {
-    NavBar,
-  }
-}
+    components: {
+      NavBar,
+    },
+  };
 </script>
 
 <style>
@@ -39,16 +43,27 @@ export default {
     height: -webkit-fill-available;
   }
 
-  html, body{ 
+  html,
+  body {
     color: #2c3e50;
     height: 100%;
     margin: 0;
     padding: 0;
   }
 
-  ::-webkit-scrollbar       {background-color:transparent;width:14px; height:16px}
-  ::-webkit-scrollbar-track {background-color:transparent;}
-  ::-webkit-scrollbar-thumb {background-color:#e7e7e7;border-radius:16px;border:4px solid #e7e7e7;}
+  ::-webkit-scrollbar {
+    background-color: transparent;
+    width: 14px;
+    height: 16px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: #e7e7e7;
+    border-radius: 16px;
+    border: 4px solid #e7e7e7;
+  }
 
   @media only screen and (max-width: 1100px) {
   }
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = defineConfig({
+  configureWebpack: {
+    plugins: [new MiniCssExtractPlugin()],
+  },
   devServer: {
     proxy: {
-      '^/api': {
-        target: 'http://olofschylander.com',
+      "^/api": {
+        target: "http://olofschylander.com",
         changeOrigin: true,
       },
-    }
-  }
-})
+    },
+  },
+});
